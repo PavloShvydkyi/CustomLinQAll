5600fe4c9191548b75c69e23c7b07a291c137381
pshvydkyi-clgx <pshvydkyi@corelogic.com>
2025-04-30 10:42:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using CustomLinQAll;
+
+var l = new List<int>();
+Console.WriteLine("empty list returns {0}", l.CastomAll((el) => { return false; }));
+int[] a = { };
+Console.WriteLine("empty array returnt {0}", a.CastomAll((el) => { return false; }));
---
+++ custom-all.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinQAll
+{
+    static class CustomAll
+    {
+        public static bool CastomAll<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+            
+            foreach (var item in source)
+            {
+                if (!predicate(item))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
